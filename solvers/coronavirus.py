d1c0ca3211447233bad03321d50fb86b50cbdd77
jal04 <josemi.bnf@outlook.es>
2020-03-13 16:06:35 +0100

    error del walkSat robado solucionado

--- solvers/coronavirus.py
+++ solvers/coronavirus.py
@@ -1,88 +1,105 @@
 #!/usr/bin/python3
-import sys
 import random
+import sys
 
-def cost(interpretation):
-    global formula
-    def isTrue(var):
-        if ( interpretation[abs(var)-1]>0 and var<0 ) or ( interpretation[abs(var)-1]<0 and var>0 ) :
-            return False
-        else:
-            return True
 
-    cost=len(formula)
-    for clausula in formula:
-        clausula_bool=False
-        for var in clausula:
-            if isTrue(var)==True:
-                cost=cost-1
-                break
-    return cost
+def parse(filename):
+    clauses = []
+    count = 0
+    for line in open(filename):
 
-def getRandomInterpretation():
-    global num_vars 
-    interpretation=[]
-    for var in range (1, num_vars+1):
-        new_var=0
-        while new_var == 0:
-            new_var=random.randrange(-1,2)*var
-        interpretation.append(new_var)
-    return interpretation
+        if line[0] == 'c':
+            continue
+        if line[0] == 'p':
+            n_vars = int(line.split()[2])
+            lit_clause = [[] for _ in range(n_vars * 2 + 1)]
+            continue
 
-def flipped(interpretation):
-    global num_vars
-    flip_var = random.randrange(0, num_vars)
-    interpretation[flip_var]*=-1
-    return interpretation
+        clause = []
+        for literal in line[:-2].split():
+            literal = int(literal)
+            clause.append(literal)
+            lit_clause[literal].append(count)
+        clauses.append(clause)
+        count += 1
+    return clauses, n_vars, lit_clause
 
-def getFormula(cnf):
-    global num_vars
-    formula=[]
-    for line in cnf:
-        if line.split()[0] is not "c":
-            if line.split()[0] is "p":
-                num_vars = int(line.split()[2])
-            else:
-                clause=[]
-                for var in line.split():
-                    if int(var) != 0:
-                        clause.append(int(var))
-                formula.append(clause)
-    return formula
 
-if __name__ == "__main__":
-    max_tries = 20000
-    max_flips = 20000
-    global num_vars
-    global formula
+def get_random_interpretation(n_vars):
+    return [i if random.random() < 0.5 else -i for i in range(n_vars + 1)]
 
-    formula = getFormula(open(sys.argv[1], "r"))
-    k = 2**num_vars#K debe ser el numero de posibles interpretaciones vecinas.
-    for i in range(1, max_tries):
-        interpretation=getRandomInterpretation()
-        cost_interpretation = cost(interpretation)
-        if cost_interpretation == 0:
-            print("C COVID-19")
-            print("s SATISFIABLE")
-            print("v "+" ".join(map(str, interpretation)))
-            exit()
-        for j in range(1, max_flips):
-            not_stop=0
-            flip_interpretation = flipped(interpretation)
-            flip_cost = cost(flip_interpretation)
-            if flip_cost < cost_interpretation:
-                #Si hemos mejorado el costo seguimos "subiendo".
-                interpretation = flip_interpretation
-                cost_interpretation = flip_cost
-                if cost_interpretation == 0:
-                    print("c COVID-19")
-                    print("s SATISFIABLE")
-                    print("v "+" ".join(map(str, flip_interpretation)))
-                    exit()
-            else:
-                #Seguimos en la misma
-                ++not_stop
-                if not_stop>k:
-                    break
-    print("c COVID-19")
-    print("s UNSATISFIABLE")
+
+def get_true_sat_lit(clauses, interpretation):
+    true_sat_lit = [0 for _ in clauses]
+    for index, clause in enumerate(clauses):
+        for lit in clause:
+            if interpretation[abs(lit)] == lit:
+                true_sat_lit[index] += 1
+    return true_sat_lit
+
+
+def update_tsl(literal_to_flip, true_sat_lit, lit_clause):
+    for clause_index in lit_clause[literal_to_flip]:
+        true_sat_lit[clause_index] += 1
+    for clause_index in lit_clause[-literal_to_flip]:
+        true_sat_lit[clause_index] -= 1
+
+
+def compute_broken(clause, true_sat_lit, lit_clause, omega=0.4):
+    break_min = sys.maxsize
+    best_literals = []
+    for literal in clause:
+
+        break_score = 0
+
+        for clause_index in lit_clause[-literal]:
+            if true_sat_lit[clause_index] == 1:
+                break_score += 1
+
+        if break_score < break_min:
+            break_min = break_score
+            best_literals = [literal]
+        elif break_score == break_min:
+            best_literals.append(literal)
+
+    if break_min != 0 and random.random() < omega:
+        best_literals = clause
+
+    return random.choice(best_literals)
+
+
+def run_sat(clauses, n_vars, lit_clause, max_flips_proportion=4):
+    max_flips = n_vars * max_flips_proportion
+    while 1:
+        interpretation = get_random_interpretation(n_vars)
+        true_sat_lit = get_true_sat_lit(clauses, interpretation)
+        for _ in range(max_flips):
+
+            unsatisfied_clauses_index = [index for index, true_lit in enumerate(true_sat_lit) if
+                                         not true_lit]
+
+            if not unsatisfied_clauses_index:
+                return interpretation
+
+            clause_index = random.choice(unsatisfied_clauses_index)
+            unsatisfied_clause = clauses[clause_index]
+
+            lit_to_flip = compute_broken(unsatisfied_clause, true_sat_lit, lit_clause)
+
+            update_tsl(lit_to_flip, true_sat_lit, lit_clause)
+
+            interpretation[abs(lit_to_flip)] *= -1
+
+
+def main():
+
+    clauses, n_vars, lit_clause = parse(sys.argv[1])
+
+    solution = run_sat(clauses, n_vars, lit_clause)
+
+    print('s SATISFIABLE')
+    print('v ' + ' '.join(map(str, solution[1:])) + ' 0')
+
+
+if __name__ == '__main__':
+    main()

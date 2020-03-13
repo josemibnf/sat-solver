ec7778ebced8a77d33ef1c84f8204214d3c8b86e
javigoo <javigo_@outlook.com>
2020-03-13 18:12:15 +0100

    Terminado rate-solvers

--- rate-solvers.sh
+++ rate-solvers.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-
 # Ejecuta race.py para todo los benchmarks con todo los solvers, y los clasifica de mejor a peor.
 
-SOLVERS=solvers/*
-BENCHMARKS=benchmark-folder/
-chmod 777 $SOLVERS
+SOLVERS=solvers/* # Directorio que contiene los solvers a evaluar
+BENCHMARKS=benchmark-folder/ # Directorio que contiene los benchmarks que realizar
 
-touch tmp-rating
+chmod 777 $SOLVERS
+touch tmp-rating.txt
 
 for s in $SOLVERS
 do
-  echo "Rating $s"
 
-  # Lo siento, esto solo pilla el nombre del solver
-  echo "" >> tmp-rating
-  echo $s | cut --complement -d/ -f 1 | cut -d. -f 1 >> tmp-rating
+  # Seleccionar el nombre del solver (lo siento)
+  solver_name=$(echo $s | cut --complement -d/ -f 1 | cut -d. -f 1)
+
+  echo "Rating $solver_name..."
 
   # Ejecuta race.py
-  ./race.py $BENCHMARKS $s | grep "Total time =" >> tmp-rating
+  solver_time=$(./race.py $BENCHMARKS $s | grep "Total time =" | cut --complement -d= -f1)
 
+  echo "$solver_name =$solver_time" >> tmp-rating.txt
 done
 
-cat tmp-rating
-rm tmp-rating
+echo -e "\n### Ranking ###\n"
+cat tmp-rating.txt | sort -n -k3 -t" "
+rm tmp-rating.txt

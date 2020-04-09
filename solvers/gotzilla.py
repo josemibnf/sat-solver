7e7bafa48b043facab821e9996bacafd9807c206
root <root@DESKTOP-CI7K1T2.localdomain>
2020-04-09 12:05:25 +0200

    gotzilla no funciona en race.py

---
+++ solvers/gotzilla.py
@@ -0,0 +1,28 @@
+import sys
+
+import single
+
+def parse(filename):
+    clauses = []
+    count = 0
+    for line in open(filename):
+
+        if line[0] == 'c':
+            continue
+        if line[0] == 'p':
+            n_vars = int(line.split()[2])
+            lit_clause = [[] for _ in range(n_vars * 2 + 1)]
+            continue
+
+        clause = []
+        for literal in line[:-2].split():
+            literal = int(literal)
+            clause.append(literal)
+            lit_clause[literal].append(count)
+        clauses.append(clause)
+        count += 1
+    return clauses, n_vars, lit_clause
+
+if __name__ == '__main__':
+    clauses, n_vars, lit_clause = parse(sys.argv[1])
+    single.run_sat(clauses, n_vars, lit_clause)

7f6917743097ec88a11c8f1f65177cf86edc47d2
root <root@DESKTOP-CI7K1T2.localdomain>
2020-04-09 18:24:48 +0200

    gotzilla train generate cnf ok

--- gotzilla-train.py
+++ gotzilla-train.py
@@ -5,9 +5,12 @@ import os
 import subprocess
 import random
 
-
-if __name__ == '__main__':
+os.system("rm -r benchmark-folder/*")
+try:
     while 1:
         rnd_cnf = "./rnd-cnf-gen-satisfiable.sh "+str(random.randrange(1,999))+" "+str(random.randrange(1,999))+" 3"
-        print(rnd_cnf)
         subprocess.call(rnd_cnf, shell=True)
+except KeyboardInterrupt:
+    pass
+
+
--- rnd-cnf-gen-satisfiable.sh
+++ rnd-cnf-gen-satisfiable.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 VAR=true
-FICHERO="benchmark-folder/hardness-$($2/$1)_$1-$2-$3.cnf"
+RATIO=$2/$1
+FICHERO="benchmark-folder/hardness-$1-$2-$3.cnf"
 echo $FICHERO
 while $VAR
 do
     ./rnd-cnf-gen.py $1 $2 $3 > $FICHERO
     RESULT=$(minisat $FICHERO | tail -1)
-    echo $RESULT
     if [ $RESULT == "SATISFIABLE" ]; then
         VAR=false
     fi
